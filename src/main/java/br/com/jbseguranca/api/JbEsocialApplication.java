8c5de5b50f8dd829520acf2e694ded7f1c40aff1
Diego Bizerra da Silva <diegorj93@gmail.com>
2024-10-04 21:22:31 -0300

    Criacao da API.

---
+++ src/main/java/br/com/jbseguranca/api/JbEsocialApplication.java
@@ -0,0 +1,13 @@
+package br.com.jbseguranca.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbEsocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbEsocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jbseguranca/api/JbEsocialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jbseguranca.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbEsocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
