3a78df7ef147397d75c6baee06f0335b9d0d5de3
Renato Areas <rsareas93@gmail.com>
2024-12-10 19:42:55 -0300

    Update Exame.java
    Mapeamento de campos e validações

--- src/main/java/br/com/jbseguranca/api/domain/Exame.java
+++ src/main/java/br/com/jbseguranca/api/domain/Exame.java
@@ -2,26 +2,30 @@ package br.com.jbseguranca.api.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Exame {
-	
-	   @JsonProperty("dtExm_21")
-       private String dtExm_21;
 
-       @JsonProperty("procRealizado_22")
-       private String procRealizado_22;
-
-       @JsonProperty("obsProc_23")
-       private String obsProc_23;
-
-       @JsonProperty("ordExame_25")
-       private String ordExame_25;
-
-       @JsonProperty("indResult_28")
-       private String indResult_28;
+	@NotNull(message = "O campo 'dtExm_21' não pode ser vazio")
+	@JsonProperty("dtExm_21")
+	private String dtExm_21;
+	@NotNull(message = "O campo 'procRealizado_22' não pode ser vazio")
+	@Size(min = 4, max = 4)
+	@JsonProperty("procRealizado_22")
+	private String procRealizado_22;
+	@Size(min = 1, max = 999)
+	@JsonProperty("obsProc_23")
+	private String obsProc_23;
+	@Size(min = 1, max = 1)
+	@JsonProperty("ordExame_25")
+	private String ordExame_25;
+	@Size(min = 1, max = 1)
+	@JsonProperty("indResult_28")
+	private String indResult_28;
 
 }
