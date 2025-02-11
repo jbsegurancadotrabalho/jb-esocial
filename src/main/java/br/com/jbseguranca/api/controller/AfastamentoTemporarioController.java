a9d841a5775b3b412f8e75c0722118d46d3fd956
Diego Bizerra da Silva <diegorj93@gmail.com>
2025-02-11 17:26:39 -0300

    Foi adicionado operation e tag do swagger

--- src/main/java/br/com/jbseguranca/api/controller/AfastamentoTemporarioController.java
+++ src/main/java/br/com/jbseguranca/api/controller/AfastamentoTemporarioController.java
@@ -1,32 +1,26 @@
 package br.com.jbseguranca.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.jbseguranca.api.domain.AfastamentoTemporario;
 import br.com.jbseguranca.api.services.AfastamentoTemporarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 
 @RestController
 @RequestMapping("/afastamentotemporario")
+@Tag(name = "Afastamento Temporario", description = "2230 - Operações de Acidente de Trabalho")
 public class AfastamentoTemporarioController {
 	
 	@Autowired
 	private AfastamentoTemporarioService afastamentoTemporarioService;
 	
-	@GetMapping()
-	public AfastamentoTemporario getAfastamentoTemporario() {
-		return afastamentoTemporarioService.consumirApiAfastamentoTrabalho();
-	}
-	
-	@GetMapping("/{id}")
-	public AfastamentoTemporario getAfastamentoTemporarioById(@PathVariable String id) {
-		return afastamentoTemporarioService.getAfastamentoTemporarioById(id);
-	}
-	
+	@PostMapping
+	@Operation(summary = "Criar Afastamento Temporario")
 	public AfastamentoTemporario createAfastamentoTemporario(AfastamentoTemporario afastamento) {
 		return afastamentoTemporarioService.createAfastamentoTemporario(afastamento);
 
--- src/main/java/br/com/jbseguranca/api/controller/SaudeTrabalhadorController.java
+++ src/main/java/br/com/jbseguranca/api/controller/SaudeTrabalhadorController.java
@@ -1,32 +1,25 @@
 package br.com.jbseguranca.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.jbseguranca.api.domain.SaudeTrabalhador;
 import br.com.jbseguranca.api.services.SaudeTrabalhadorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/saudetrabalhador")
+@Tag(name = "Saude Trabalhador", description = "2210 - Operações de Acidente de Trabalho")
 public class SaudeTrabalhadorController {
 	
 	@Autowired
 	private SaudeTrabalhadorService saudeTrabalhadorService;
 	
-	@GetMapping()
-	public SaudeTrabalhador getSaudeTrabalhador() {
-		return saudeTrabalhadorService.consumirApiSaudeTrabalhador();
-	}
 	
-	@GetMapping("/{id}")
-	public SaudeTrabalhador getSaudeTrabalhadorById(@PathVariable String Id) {
-		return saudeTrabalhadorService.getSaudeTrabalhadorById(Id);
-	}
-	
+	@Operation(summary = "Criar Saude Trabalhador")
 	@PostMapping
 	public SaudeTrabalhador createSaudeTrabalhador(SaudeTrabalhador trabalhador) {
 		return saudeTrabalhadorService.createSaudeTrabalhadorById(trabalhador);
