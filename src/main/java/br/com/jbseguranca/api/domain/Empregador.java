43d61f4a0210105ff74d0e5671edecb5dfd8bd26
Renato Areas <rsareas93@gmail.com>
2025-02-24 21:50:19 -0300

    Ajustes em endpoint e mapeamento de endereço para cadastrar empregador

--- src/main/java/br/com/jbseguranca/api/domain/Empregador.java
+++ src/main/java/br/com/jbseguranca/api/domain/Empregador.java
@@ -1,13 +1,16 @@
 package br.com.jbseguranca.api.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class Empregador {
 
+	@NotNull(message = "O CPF ou o CNPJ do empregador tem que ser informado")
     @JsonProperty("empregador")
-    private String empregador;
+    private String empregador = "66231931000109";
 
     @JsonProperty("email")
     private String email;
@@ -36,6 +39,7 @@ public class Empregador {
     @JsonProperty("uf")
     private String uf;
 
+    @NotNull(message = "A Razão social tem que ser informada.")
     @JsonProperty("razaosocial")
     private String razaosocial;
 }
--- src/main/java/br/com/jbseguranca/api/services/EmpregadorService.java
+++ src/main/java/br/com/jbseguranca/api/services/EmpregadorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,49 +15,62 @@ import br.com.jbseguranca.api.exception.JbException;
 
 @Service
 public class EmpregadorService {
-	
-	@Value("${endereco.tecnospeed.api.url}")
+
+	@Value("${cadastrar.empregador.tecnospeed.api.url}")
 	private String apiUrl;
-	
+
 	@Value("${tecnospeed.auth.token}")
 	private String apiToken;
 
 	@Value("${tecnospeed.auth.cnpj}")
 	private String cnpjSh;
-	
+
 	private final RestTemplate restTemplate;
-	
-		public EmpregadorService(RestTemplate restTemplate) {
+
+	public EmpregadorService(RestTemplate restTemplate) {
 		this.restTemplate = restTemplate;
+	}
+
+	public Empregador consumirApiEmpregador() {
+		try {
+			return restTemplate.getForObject(apiUrl, Empregador.class);
+		} catch (RestClientException ex) {
+			ex.printStackTrace();
+			throw new JbException("Erro ao consumir a API de para cadastro de empregador.", ex);
+		}
+
 	}
 
 	public EmpregadorDto cadastrarEmpregador(Empregador empregador) {
 		if (apiUrl == null || apiUrl.isEmpty()) {
-	        throw new JbException("A URL da API não está configurada.");
-	    }
+			throw new JbException("A URL da API não está configurada.");
+		}
 
-	    try {
-	        // Serializando o objeto para JSON
-	        ObjectMapper objectMapper = new ObjectMapper();
-	        String json = objectMapper.writeValueAsString(empregador);
+		try {
+			// Serializando o objeto para JSON
+			ObjectMapper objectMapper = new ObjectMapper();
+			String json = objectMapper.writeValueAsString(empregador);
 
-	        // Fazendo a requisição POST
-	        ResponseEntity<String> response = restTemplate.postForEntity(apiUrl, json, String.class);
+			// Fazendo a requisição POST
+			ResponseEntity<String> response = restTemplate.postForEntity(apiUrl, json, String.class);
 
-	        if (response.getStatusCode().is2xxSuccessful()) {
-	            // Converte a resposta para o DTO
-	        	//log.info("Lote Recebido com Sucesso.", response.getBody());
-	            return objectMapper.readValue(response.getBody(), EmpregadorDto.class);
-	        } else {
-	            throw new JbException("Erro na API: Código " + response.getStatusCodeValue());
-	        }
-	    } catch (HttpClientErrorException ex) {
-	        //log.error("Erro ao criar Monitoramento da saúde do Trabalhor: {}", ex.getResponseBodyAsString());
-	        throw new JbException("Erro ao criar o empregador: " + ex.getMessage(), ex);
-	    } catch (Exception ex) {
-	        //log.error("Erro inesperado ao criar Monitoramento da saúde do Trabalhor.", ex);
-	        throw new JbException("Erro inesperado ao criar um empregador.", ex);
-	    }
-	
-}
+			if (response.getStatusCode().is2xxSuccessful()) {
+				// Converte a resposta para o DTO
+				// log.info("Lote Recebido com Sucesso.", response.getBody());
+				return objectMapper.readValue(response.getBody(), EmpregadorDto.class);
+			} else {
+				throw new JbException("Erro na API: Código " + response.getStatusCodeValue());
+			}
+		} catch (HttpClientErrorException ex) {
+			// log.error("Erro ao criar Monitoramento da saúde do Trabalhor: {}",
+			// ex.getResponseBodyAsString());
+			throw new JbException("Erro ao criar o empregador: " + ex.getMessage(), ex);
+		} catch (Exception ex) {
+			// log.error("Erro inesperado ao criar Monitoramento da saúde do Trabalhor.",
+			// ex);
+			throw new JbException("Erro inesperado ao criar um empregador.", ex);
+		}
+
+	}
+
 }
